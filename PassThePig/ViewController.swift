f72a12afdbdd26fe9c3f4845e13a46321dd09eb1
Avery McAllister <avery@bradandsylver.com>
2023-12-05 09:21:58 -0600

    Initial Commit

---
+++ PassThePig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassThePig
+//
+//  Created by Avery McAllister on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
